15e4f905eb4950ac0209725218f5e37317ccae60
Andrei Horak <linkyndy@gmail.com>
2017-11-29 12:49:21 +0200

    Add control tower supervisor

--- lib/air_traffic_control.ex
+++ lib/air_traffic_control.ex
@@ -2,10 +2,26 @@ defmodule AirTrafficControl do
   use Application
 
   def start(:normal, args) do
-    :ok
+    supervisor = AirTrafficControl.ControlTowerSupervisor.start_link(args)
+    airports = Application.get_env(:air_traffic_control, :airports)
+
+    airports
+      |> List.foldl([], fn {name, count}, acc -> acc ++ open_landing_strips(name, count) end)
+
+    supervisor
   end
 
   def stop(_) do
     :ok
+  end
+
+  defp open_landing_strips(name, count) do
+    AirTrafficControl.ControlTowerSupervisor.start_control_tower(name)
+    open_landing_strips(name, count, [])
+  end
+
+  defp open_landing_strips(_name, 0, acc), do: acc
+  defp open_landing_strips(name, count, acc) do
+    open_landing_strips(name, count - 1, acc ++ [AirTrafficControl.ControlTower.open_landing_strip(name)])
   end
 end
---
+++ lib/air_traffic_control/control_tower_supervisor.ex
@@ -0,0 +1,18 @@
+defmodule AirTrafficControl.ControlTowerSupervisor do
+  use Supervisor
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  def start_control_tower(name) do
+    Supervisor.start_child(__MODULE__, [name])
+  end
+
+  def init(_opts) do
+    children = [
+      worker(AirTrafficControl.ControlTower, [])
+    ]
+    Supervisor.init(children, strategy: :simple_one_for_one)
+  end
+end
