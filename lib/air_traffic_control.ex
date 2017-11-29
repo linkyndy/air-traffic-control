1e05a329409b437510671d0904e36ab310975f43
Andrei Horak <linkyndy@gmail.com>
2017-11-29 10:55:03 +0100

    Initial project

---
+++ lib/air_traffic_control.ex
@@ -0,0 +1,18 @@
+defmodule AirTrafficControl do
+  @moduledoc """
+  Documentation for AirTrafficControl.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AirTrafficControl.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/air_traffic_control_test.exs
@@ -0,0 +1,8 @@
+defmodule AirTrafficControlTest do
+  use ExUnit.Case
+  doctest AirTrafficControl
+
+  test "greets the world" do
+    assert AirTrafficControl.hello() == :world
+  end
+end
